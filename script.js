891e7457686104bac43e922bd40de4bf9c92a369
laminelawal <macbookpro@lamine.fritz.box>
2024-05-15 02:37:48 +0200

    New features edit/delete/erros-alert

--- script.js
+++ script.js
@@ -9,17 +9,19 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 const handleClearBtn = document.querySelector('.clear_workouts');
-
+const alertMsgContainer = document.querySelector('.alert__msg');
 //This is the parent of all workout child
+
 class Workout {
   date = new Date();
-  id = (Date.now() + '').slice(-10);
+  _id = (Date.now() + '').slice(-10);
   constructor(coords, distance, duration) {
     this.coords = coords;
     this.distance = distance;
     this.duration = duration;
-    console.log(this.id);
+    console.log(this._id);
   }
+
   _setDescription() {
     // prettier-ignore
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
@@ -69,6 +71,7 @@ class App {
   //Here i will create public fields, not private like the course!
   map;
   markEvent;
+  _username;
   /**Here im loading all logic when the app is starting
    * First one : to get the position
    * Second one: to toggle the cadence/elevationGian while inputType is changed
@@ -77,14 +80,55 @@ class App {
    * Fifth one: Im creating a general workouts array to use in certain places
    */
   constructor() {
+    this._username = null;
+    this._initializeUsername();
     this._getPosition();
     inputType.addEventListener('change', this._toggleElevationField);
-    form.addEventListener('submit', this._newWorkOut.bind(this));
     containerWorkouts.addEventListener('click', this._gotToPopup.bind(this));
     this.workouts = [];
     //Get data from local storage
     this._getLocalStorage();
     handleClearBtn.addEventListener('click', this._reset.bind(this));
+    //Comment this and recomment the below one:
+    this._getOnSubmit();
+    // form.addEventListener('submit', this._newWorkOut.bind(this));
+  }
+
+  //Here i use this to test some logics but u can comment this and recomment the  one in the constructor
+  _getOnSubmit() {
+    form.addEventListener('submit', this._newWorkOut.bind(this));
+  }
+  // Call this method in your constructor or wherever necessary
+  _initializeUsername() {
+    // Check if the username exists in sessionStorage
+    const storedUsername = sessionStorage.getItem('_username');
+
+    if (storedUsername) {
+      // If username exists, set it to the instance property
+      this._username = storedUsername;
+      // Get reference to the userdata_welcome element
+      const usrMsgCont = document.querySelector('.userdata_welcome');
+      if (usrMsgCont) {
+        // Remove the active class if userdata_welcome element exists
+        usrMsgCont.classList.remove('userdata_welcome--active');
+      }
+    } else {
+      // If username does not exist, set it using prompt and save to sessionStorage
+      this._setUserSession();
+    }
+  }
+
+  _setUserSession() {
+    const username = prompt('Please write your name to make the app correctly');
+    if (username) {
+      this._username = username;
+      this._dataSession(username);
+    }
+  }
+
+  _dataSession(username) {
+    if (!username) return;
+    sessionStorage.setItem('_username', username);
   }
 
   //Qua otteniamo la positione dall'utente appena entrato nel sito.
@@ -151,6 +195,15 @@ class App {
     }
   }
   /**Here im creating all logic to create a new object by the target type */
+
+  _showError() {
+    alertMsgContainer.classList.add('alert__msg--active');
+    alertMsgContainer.innerHTML = `${this._username} Please write a positive number 😅`;
+
+    const timer = setTimeout(() => {
+      alertMsgContainer.classList.remove('alert__msg--active');
+    }, 3800);
+  }
   _newWorkOut(e) {
     e.preventDefault();
     //Here there are variable to store the input value from the form
@@ -159,7 +212,6 @@ class App {
     const distance = +inputDistance.value;
     const { lat, lng } = this.markEvent.latlng;
     var workout;
-
     const allInputAreNumber = (...inputs) => {
       return inputs.every(num => Number.isFinite(num));
     };
@@ -176,8 +228,7 @@ class App {
         !allInputAreNumber(duration, distance, cadence) ||
         !allIsPositiveNum(duration, distance, cadence)
       )
-        return alert('Write a positive number');
-
+        return this._showError();
       workout = new Running([lat, lng], duration, distance, cadence);
       console.log(workout);
       //cheking if all logics are working
@@ -193,7 +244,7 @@ class App {
         !allInputAreNumber(duration, distance, elevation) ||
         !allIsPositiveNum(duration, distance, elevation)
       )
-        return alert('Write a positive number');
+        return this._showError();
 
       workout = new Cycling([lat, lng], duration, distance, elevation);
       //cheking if all logics are working
@@ -201,7 +252,7 @@ class App {
     }
     //Add new workout created to wourkouts array
     this.workouts.push(workout);
-    console.log(this.workouts);
+    console.log('All workouts', this.workouts);
     //Render workout on map as a marker
     this._renderWorkoutMarker(workout);
 
@@ -209,8 +260,8 @@ class App {
     // console.log(iputType, cadence, duration, elevetion);
     this._renderWorkout(workout);
     this._hideForm();
-
     this._setLocalStorage();
+    location.reload();
   }
   _renderWorkoutMarker(workout) {
     // const { lat, lng } = this.markEvent.latlng;
@@ -233,7 +284,15 @@ class App {
     let html;
 
     html = `
-    <li class="workout workout--running" data-id="${workout.id}">
+    <li class="workout workout--running" data-id="${workout._id}">
+    <div class="control__workouts">
+    <button class="control__edit " type="submit" data-editid="${
+      workout._id
+    }">edit</button>
+    <button class="control__delete " data-delid="${
+      workout._id
+    }" type="submit"">delete</button>
+    </div>
     <h2 class="workout__title">${workout.description}</h2>
     <div class="workout__details">
       <span class="workout__icon">${
@@ -279,14 +338,21 @@ class App {
         </li>`;
     }
     form.insertAdjacentHTML('afterend', html);
+
+    //Controlling the workouts array btns
+    const allBtn = document.querySelector('.control__workouts');
+    const deletCOnt = allBtn.closest('.control__workouts');
+    deletCOnt.addEventListener('click', e => this._deleteSingleWorkout(e));
+    deletCOnt.addEventListener('click', e => this._editWorkout(e));
   }
+
   //Here when user click any workout div to go automatically to the clicked workout on the map
   _gotToPopup(e) {
     const workIndex = e.target.closest('.workout');
     //Here cheking if it not the current target cliked to avoid it, so we will not have many repetitions.
     if (!workIndex) return;
     //Here creating a finded workout cliked and then setting the set view on the map marker.
-    const workoute = this.workouts.find(a => a.id === workIndex.dataset.id);
+    const workoute = this.workouts.find(a => a._id === workIndex.dataset.id);
     map.setView(workoute.coords, 30);
   }
 
@@ -305,10 +371,84 @@ class App {
       this._renderWorkout(el);
     });
   }
-
+  //Here to reset/delete all data/workouts
   _reset() {
     localStorage.removeItem('_workouts');
     location.reload();
+  }
+
+  _deleteSingleWorkout(e) {
+    if (e.target.classList.contains('control__delete')) {
+      console.log('Delete target', e);
+      const delid = e.target.dataset.delid;
+
+      if (!delid) return;
+      console.log('Delid', delid);
+      const updateWorkouts = this.workouts.filter(el => el._id !== delid);
+      this.workouts = updateWorkouts;
+      console.log('New Array', this.workouts);
+      const workouteupdated = this.workouts;
+      // console.log('New workout', workouteupdated);
+      location.reload();
+      this._setLocalStorage(updateWorkouts);
+      console.log(workouteupdated.length);
+      if (workouteupdated >= 0) {
+        const check = (workouteupdated.length = 0);
+        console.log(check);
+        localStorage.removeItem('_workouts');
+      }
+    }
+  }
+  _editWorkout(e) {
+    if (e.target.classList.contains('control__edit'))
+      console.log('Target edit', e.target);
+    const editId = e.target.dataset.editid; // Retrieve workout ID from dataset
+    const workoutToEdit = this.workouts.find(workout => workout._id === editId); // Find workout to edit
+    const [lat, lng] = workoutToEdit.coords;
+    L.marker([lat, lng], { draggable: false })
+      .addTo(map)
+      .bindPopup(
+        L.popup({
+          closeButton: false,
+          autoClose: false,
+          closeOnEscapeKey: false,
+          closeOnClick: false,
+          className: `${workoutToEdit.type}-popup`,
+        })
+      )
+      .setPopupContent(workoutToEdit.description)
+      .openPopup();
+    if (!workoutToEdit) return;
+
+    inputType.value = workoutToEdit.type;
+    inputDistance.value = +workoutToEdit.distance;
+    inputDuration.value = +workoutToEdit.duration;
+
+    const editedWorkout = {
+      ...workoutToEdit,
+      type: inputType.value,
+      distance: +inputDistance.value,
+      duration: +inputDuration.value,
+      coords: workoutToEdit.coords,
+    };
+
+    if (inputType.value === 'running') {
+      inputCadence.value = +workoutToEdit.cadence;
+    } else if (inputType.value === 'cycling') {
+      inputElevation.value = +workoutToEdit.elevatioGain;
+    }
+    // Show form for editing
+    this._showForm();
+    // Remove the workout from the workouts array
+    this.workouts.push(editedWorkout);
+    const updateEdit = this.workouts.filter(workout => workout._id !== editId);
+    this.workouts = updateEdit;
+    const finalEdit = this.workouts;
+
+    this.workouts.slice(finalEdit, 1);
+    console.log('Final', finalEdit);
+    console.log('ALL ', this.workouts);
+    this._setLocalStorage();
   }
 }
 
